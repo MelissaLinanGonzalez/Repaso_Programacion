b85e80198ab4be7f9fdc46e2d4980bc29bdfad1d
Melissa Liñán <melissa16800@gmail.com>
2025-06-05 17:24:27 +0200

    Ejemplo con ciclo do while

---
+++ Ciclos/CicloDoWhile.java
@@ -0,0 +1,10 @@
+public class CicloDoWhile {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo Do While ***");
+
+        int contador = 1;
+        do {
+            System.out.println(contador++);
+        } while (contador <= 10);
+    }
+}
