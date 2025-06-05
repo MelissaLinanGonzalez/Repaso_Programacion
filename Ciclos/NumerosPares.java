5738de00670db4ecadf531b7bcb2a16d06317f11
Melissa Liñán <melissa16800@gmail.com>
2025-06-05 17:21:18 +0200

    Ejemplo con while mostrando numeros pares

---
+++ Ciclos/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        System.out.println("*** Numeros Pares del 1 al 20 ***");
+        int contador = 1;
+        while (contador <=20){
+            if (contador % 2 == 0){
+                System.out.print(contador + " ");
+            }
+            contador++;
+        }
+    }
+}
