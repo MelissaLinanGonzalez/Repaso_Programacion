b6d6acca82e331cacd26128d9135836d389029ec
Melissa Liñán <melissa16800@gmail.com>
2025-06-05 18:35:05 +0200

    Mejora del juego de adivinar numero

--- Ciclos/JuegoAdivinanzas.java
+++ Ciclos/JuegoAdivinanzas.java
@@ -11,19 +11,24 @@ public class JuegoAdivinanzas {
         System.out.println("!Intenta adivinar el número secreto, entre 1 y 50!");
         boolean seguir = false;
         while (!seguir){
-            System.out.print("Introduce el número que creas que es: ");
-            int num = entrada.nextInt();
-            if (num == numSecreto){
-                intentos++;
-                System.out.println("Enhorabuena! has hacertado el número: " + numSecreto);
-                System.out.println("Lo has conseguido en " + intentos + " intentos");
-                seguir = true;
-            } else if (numSecreto > num){
-                System.out.println("El número buscado es mayor al introducido \n");
-                intentos++;
+            if (intentos < 10){
+                System.out.print("Introduce el número que creas que es: ");
+                int num = entrada.nextInt();
+                if (num == numSecreto){
+                    intentos++;
+                    System.out.println("Enhorabuena! has hacertado el número: " + numSecreto);
+                    System.out.println("Lo has conseguido en " + intentos + " intentos");
+                    seguir = true;
+                } else if (numSecreto > num){
+                    System.out.println("El número buscado es mayor al introducido \n");
+                    intentos++;
+                } else {
+                    System.out.println("El número introducido es mayor al buscado \n");
+                    intentos++;
+                }
             } else {
-                System.out.println("El número introducido es mayor al buscado \n");
-                intentos++;
+                System.out.println("Has sobrepasado el número de intentos posibles: " + intentos);
+                seguir = true;
             }
         }
     }
