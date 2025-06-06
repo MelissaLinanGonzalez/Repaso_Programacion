bd79fa98f40615f25ed3d0f9bebf5b64506ee250
Melissa Liñán <melissa16800@gmail.com>
2025-06-06 13:05:11 +0200

    Introducción a arrays

---
+++ Arrays/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Arrays
+        // 1. Declaramos un array
+        int[] enteros;
+
+        // 2. Inicializar el array
+        enteros = new int[3]; // Se reservan 3 espacios en la memoria para el array
+
+        // 2. Declarar e inicializar un array
+        int[] numeros = new int[2];
+        
+    }
+}
