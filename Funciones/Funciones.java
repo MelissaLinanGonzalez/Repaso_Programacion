0410cb9ba442c7fa4f9deb99e381a9df7faa27bb
Melissa Liñán <melissa16800@gmail.com>
2025-06-07 14:14:55 +0200

    Inicio de funciones

---
+++ Funciones/Funciones.java
@@ -0,0 +1,11 @@
+public class Funciones {
+
+    static void saludar(String nombre){
+        System.out.println("Hola " + nombre);
+    }
+
+    public static void main(String[] args) {
+        saludar("Pepe");
+        
+    }
+}
