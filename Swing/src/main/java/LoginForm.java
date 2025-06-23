b4248cc250bc12299db907b33181cbd9d966d09b
Melissa Liñán <melissa16800@gmail.com>
2025-06-23 19:30:53 +0200

    Ventana de Login en modo oscuro

--- Swing/src/main/java/LoginForm.java
+++ Swing/src/main/java/LoginForm.java
@@ -1,8 +1,27 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame{
     private JPanel panelPrincipal;
     private JTextField usuarioTexto;
     private JPasswordField passwordTexto;
     private JButton enviarBoton;
+
+    public LoginForm(){
+        inicializarForma();
+    }
+
+    private void inicializarForma(){
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null); // Se centra la ventana
+    }
+
+    public static void main(String[] args) {
+        FlatDarculaLaf.setup(); // Modo de vista oscuro
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+    }
 }
