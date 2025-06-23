0d128cf8a6a1b63a6bff1fd2c61b1c7ff97234b9
Melissa Liñán <melissa16800@gmail.com>
2025-06-23 19:14:56 +0200

    Implementación de escucha activa de teclado

--- Swing/src/main/java/Forma.java
+++ Swing/src/main/java/Forma.java
@@ -3,6 +3,8 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Forma extends JFrame{ // Se extiende para poder trabajar con Swing y usar los métodos sin problemas
     private JPanel panelPrincipal;
@@ -11,8 +13,15 @@ public class Forma extends JFrame{ // Se extiende para poder trabajar con Swing 
 
     public Forma(){
         inicializarForma();
-        campoTexto.addActionListener(e -> {
-            System.out.println("Se ejecutó el action listener");
+//        campoTexto.addActionListener(e -> {
+//            replicarTexto();
+//        });
+        campoTexto.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                replicarTexto();
+            }
         });
     }
 
@@ -21,6 +30,10 @@ public class Forma extends JFrame{ // Se extiende para poder trabajar con Swing 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Cuando se cierre la ventana tambien finaliza la aplicacion
         setSize(600, 400);
         setLocationRelativeTo(null); // Se centra la venta
+    }
+
+    private void replicarTexto(){
+        this.replicadorLabel.setText(this.campoTexto.getText()); // Se recupera el texto introducido en el campo de texto
     }
 
     public static void main(String[] args) {
